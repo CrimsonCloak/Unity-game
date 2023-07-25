268c772e0a04e617f2322fda0a2b2b2b22998f8f
CrimsonCloak <alexander.veldeman.999@gmail.com>
2023-07-25 17:47:31 +0200

    Initial commit

---
+++ Assets/FirstScript.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstScript : MonoBehaviour
+{      
+    public float moveSpeed = 5f;
+    public float jumpStrength = 5f;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private const float groundCheckRadius = 0.2f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+        // STUFF FOR MOVEMENT
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(horizontalInput, verticalInput).normalized;
+
+        rb.velocity = movement * moveSpeed;
+        // // Rotation testing
+        // if (movement != Vector2.zero)
+        // {
+        //     float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
+        //     transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        // }
+
+
+        // STUFF FOR JUMPING
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            // Apply jump force to the Rigidbody
+            rb.AddForce(new Vector2(0f, jumpStrength), ForceMode2D.Impulse);
+        }
+
+    }
+}
