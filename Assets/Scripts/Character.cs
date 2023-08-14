0b89bfafdadba1fa3e66f572166696d84203684d
CrimsonCloak <alexander.veldeman.999@gmail.com>
2023-08-14 17:40:39 +0200

    Added basic ladderpole and movement around ladders

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -13,24 +13,25 @@ public class Character : MonoBehaviour
     public int NegativeGravityStrength = -8;
     public int PositiveGravityStrength = 8;
     public int Coins = 0;
-
-
-
+    private float horizontalInput;
+    private Transform position;
     private Rigidbody2D rb;
     private bool isGrounded;
+    private float climbSpeed = 5.0f;
     private const float groundCheckRadius = 0.2f;
     // private bool normalGravity = true;
     // private bool isJumping = false;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        position = GetComponent<Transform>();
     }
 
     void Update()
 
     {
         // STUFF FOR MOVEMENT
-        float horizontalInput = Input.GetAxis("Horizontal");
+        horizontalInput = Input.GetAxis("Horizontal");
 
 
         Vector2 movement = new Vector2(horizontalInput, 0).normalized;
@@ -88,10 +89,20 @@ public class Character : MonoBehaviour
 
         // Collision and mechanics for being on ladders
 
+
+        // Remove gravity while on ladder object
         if (collision.gameObject.tag == "Ladder")
         {
             rb.gravityScale = 0;
             // Debug.Log("Collison!");
+
+            float verticalInput = Input.GetAxis("Vertical");
+            float translation = verticalInput * climbSpeed;
+            translation *= Time.deltaTime;
+            position.Translate(0, translation, 0);
+
+
+
         }
     }
         private void OnTriggerExit2D(Collider2D collision) 
@@ -99,6 +110,7 @@ public class Character : MonoBehaviour
 
         // Collision and mechanics for exiting off ladders
 
+        // Return gravity upon leaving ladder object
         if (collision.gameObject.tag == "Ladder")
         {
             rb.gravityScale = PositiveGravityStrength;
